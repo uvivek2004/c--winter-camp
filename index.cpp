227d38b16420fb9455eb6229628d3be998f1f873
uvivek2004 <22bcs11046@cuchd.in>
2024-12-23 11:37:50 +0530

    class

---
+++ index.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a ;
+    int b;
+    cout<<"enter the two number a nad b:";
+    cin>>a;
+    cin>>b;
+    cout<<"adding of two number "<<a+b;
+}+
